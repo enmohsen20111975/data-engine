5fd1c1f5760de9de412177bab4f2018672f4f9fe
Z User <z@container>
2026-05-31 20:31:38 +0000

    Use absolute path for Python venv interpreter

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -23,7 +23,7 @@ module.exports = {
       watch: false,
       autorestart: true,
       max_restarts: 10,
-      interpreter: 'venv/bin/python',
+      interpreter: '/home/meme/my apps/unused/Data engine/mini-services/scraper-service/venv/bin/python',
       env: {
         PYTHONUNBUFFERED: '1'
       }
